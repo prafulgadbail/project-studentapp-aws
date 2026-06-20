aaa21fdc34519536dc4dcf6329ab2acac2db6a6a
Praful Gadbail <praful.gadbail01@gmail.com>
2026-06-20 11:21:40 +0000

    devops: add docker configuration and environment templates

--- frontend/src/api/userService.js
+++ frontend/src/api/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://54.91.185.15:8080/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const fetchUsers = async () => {
   try {
